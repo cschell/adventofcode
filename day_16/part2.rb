884de566eb423be2ea62f341e155e41d4210d29b
Christian Schell <mail@chrisschell.de>
2015-12-18 16:55:15 +0100

    Solve day 16 part 2

--- day_16/part2.rb
+++ day_16/part2.rb
@@ -1,6 +1,23 @@
 require "./part1"
 
 class Part2 < Part1
-  def result
+
+
+end
+class Sue
+  def matches?(things)
+    things.each do |key, value|
+      if self.things[key]
+        case key
+        when "cats", "trees"
+          return false if value >= self.things[key]
+        when "pomeranians", "goldfish"
+          return false if value <= self.things[key]
+        else
+          return false if value != self.things[key]
+        end
+      end
+    end
+    true
   end
 end
--- day_16/run.rb
+++ day_16/run.rb
@@ -1,15 +1,14 @@
 #!/usr/bin/env ruby
 
-require "./part1"
-require "./part2"
-
 File.open("input.txt") do |file|
   lines = file.each_line.to_a
 
+  require "./part1"
   part1 = Part1.new(lines)
   result1 = part1.result
   puts "Result part1: #{result1}"
 
+  require "./part2"
   part2 = Part2.new(lines)
   result2 = part2.result
   puts "Result part2: #{result2}"
